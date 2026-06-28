c6f47836b7afa68d947adc9fd6d231115dcfd36c
fujibussan-sys <fujibussan.tsumura@gmail.com>
2026-06-28 19:42:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *  ・このファイルは / (ルート) に置いてください
  * ===================================================== */
 
-const CACHE_NAME   = 'chatone-pwa-v9';
+const CACHE_NAME   = 'chatone-pwa-v10';
 const ASSETS = [
   './',
   './index.html',
